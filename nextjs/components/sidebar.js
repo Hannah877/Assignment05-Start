7e98d849b9bb5b9fc8f52110a00d0b3b5f35614c
Yihan zhang <yihanzhang77@gmail.com>
2024-04-20 23:57:20 -0500

    UI changes

---
+++ nextjs/components/sidebar.js
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+
+const Sidebar = () => {
+  return (
+    <div className="w-64 bg-black bg-opacity-40 text-white flex flex-col">
+      <div className="flex flex-col items-center py-10">
+        <h1 className="text-xl font-bold mb-10">DVD Rental</h1>
+        <nav className="flex flex-col w-full px-5">
+          <Link href="/" className="mb-5 text-gray-300 hover:text-gray-100">Home</Link>
+          <Link href="/rental" className="text-gray-300 hover:text-gray-100">Rent</Link>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -1,19 +1,49 @@
 import Layout from "../components/layout";
 import Link from 'next/link';
+import Image from 'next/image';
+import Sidebar from "../components/sidebar";
 
 export default function Home() {
+  const movies = [
+    { title: "Movie 1", imageUrl: "/image1.png" },
+    { title: "Movie 2", imageUrl: "/image2.png" },
+    { title: "Movie 3", imageUrl: "/image3.png" },
+    { title: "Movie 4", imageUrl: "/image4.png" },
+  ];
+
   return (
-    <Layout>
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Welcome to our DVD Rental Store.</h1>
-      <Link href='/rental'>
-        <div className="mt-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Rent your favorite videos today
+    <div className="flex min-h-screen bg-gray-800">
+      <Sidebar />
+
+      <div className="flex-1">
+
+        <div className="relative w-full h-[300px]">
+          <Image
+            src="/background1.png"
+            alt="Featured Background"
+            layout="fill"
+            objectFit="cover"
+            quality={100}
+          />
+          <div className="absolute inset-0 flex items-center justify-center"> 
+            <span className="text-gray-200 text-3xl">Discover and Rent your Favorite Videos Today!</span>
+          </div>
         </div>
-      </Link>
-    </main>
-  </Layout>
+      
+        <section className="mb-2 p-5 bg-gray-800">
+          <h2 className="text-xl font-bold mb-4 text-white">Trending</h2>
+          <div className="w-4"></div>
+          <div className="flex space-x-4 overflow-auto">
+            {movies.map((movie, index) => (
+              <div key={index} className="flex-none w-[290px] h-[370px] relative rounded-lg border border-white">
+                <Image src={movie.imageUrl} alt={movie.title} layout="fill" objectFit="cover" className="rounded-lg"/>
+              </div>
+            ))}
+          </div>
+        </section>
+
+      </div>
+      
+    </div>
   );
 }
-
-
